f95c890daad2084ad8877840401bc1a4d6e16020
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-04 09:26:30 +0700

    Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as compression from 'compression';
 import * as passport from 'passport';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -23,6 +25,14 @@ async function bootstrap() {
     origin: '*',
   });
   app.use(passport.initialize());
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(envConfig.port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
