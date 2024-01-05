e5a78234255087decdcd3171f34388bec0be3ca4
minhnhat165 <nguyenminhnhat301101@gmail.com>
2024-01-05 13:05:58 +0700

    Update environment and notification configurations

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { JwtUserId } from 'src/common/decorators';
 import { SubscribeDto } from './dto/subscribe.dto';
+import { CheckSubscribedDto } from './dto/check-subscribed.dto';
 
 @Controller('notifications')
 export class NotificationController {
@@ -19,5 +20,30 @@ export class NotificationController {
         message: 'Subscribe successfully',
       };
     }
+  }
+  // get all subscriptions of user
+  @Get('/devices')
+  async getSubscriptions(@JwtUserId() userId: string) {
+    const subscriptions = await this.notificationService.getDevices(userId);
+    return {
+      data: subscriptions,
+      message: 'Get subscriptions successfully',
+    };
+  }
+
+  // check if user has subscribed to notification
+  @Post('/check')
+  async checkSubscribed(
+    @JwtUserId() userId: string,
+    @Body() checkSubscribedDto: CheckSubscribedDto,
+  ) {
+    const isSubscribed = await this.notificationService.checkSubscription(
+      userId,
+      checkSubscribedDto.token,
+    );
+    return {
+      data: isSubscribed,
+      message: 'Check subscribed successfully',
+    };
   }
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-// import * as firebase from 'firebase-admin';
+import * as firebase from 'firebase-admin';
 import { Notification } from './schemas/notifications.schema';
 import { envConfig } from 'src/configs/env.config';
 
-// firebase.initializeApp({
-//   credential: firebase.credential.cert({
-//     clientEmail: envConfig.firebase.credentials.clientEmail,
-//     privateKey: envConfig.firebase.credentials.privateKey.replace(/\\n/g, '\n'),
-//     projectId: envConfig.firebase.credentials.projectId,
-//   }),
-// });
+firebase.initializeApp({
+  credential: firebase.credential.cert({
+    clientEmail: envConfig.firebase.credentials.clientEmail,
+    privateKey: envConfig.firebase.credentials.privateKey.replace(/\\n/g, '\n'),
+    projectId: envConfig.firebase.credentials.projectId,
+  }),
+});
 @Injectable()
 export class NotificationService {
   constructor(
@@ -30,17 +30,30 @@ export class NotificationService {
       return acc;
     }, [] as string[]);
 
-    console.log(tokens);
-
     try {
-      // const response = await firebase.messaging().sendEachForMulticast({
-      //   tokens,
-      //   notification: {
-      //     title,
-      //     body,
-      //   },
-      // });
-      // console.log(response);
+      const response = await firebase.messaging().sendEachForMulticast({
+        tokens,
+        notification: {
+          title,
+          body,
+        },
+      });
+      console.log(
+        response.responses.forEach(async (res, index) => {
+          //if error is entitity not found, remove token from database
+          if (
+            res.error?.code === 'messaging/invalid-registration-token' ||
+            res.error?.code === 'messaging/registration-token-not-registered'
+          ) {
+            const token = tokens[index];
+            console.log(token);
+            await this.notificationModel.updateOne(
+              { tokens: { $in: [token] } },
+              { $pull: { tokens: token } },
+            );
+          }
+        }),
+      );
     } catch (error) {
       console.log(error);
     }
@@ -56,7 +69,22 @@ export class NotificationService {
       await newNotification.save();
       return;
     }
-    notification.tokens.push(token);
-    await notification.save();
+    await notification.updateOne({ $addToSet: { tokens: token } });
+  }
+
+  async getDevices(userId: string) {
+    const notification = await this.notificationModel.findOne({ userId });
+    if (!notification) {
+      return [];
+    }
+    return notification.tokens;
+  }
+
+  async checkSubscription(userId: string, token: string) {
+    const notification = await this.notificationModel.findOne({
+      userId,
+      tokens: { $in: [token] },
+    });
+    return !!notification;
   }
 }
