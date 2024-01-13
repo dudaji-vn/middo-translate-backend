23b77796959401590d346ddf0634656970b9891c
SeokHwan Hong <shhong@dudaji.com>
2024-01-13 18:34:49 +0900

    Implement experimental cli

---
+++ sandbox/cli/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const commander = require("commander");
+const program = new commander.Command();
+
+const { getUsers } = require('./users.js');
+const { get } = require("http");
+
+program
+    .name('middo-cli')
+    .description('CLI for middo')
+    .version('0.0.1');
+
+program.command('get-users')
+    .description('Get users')
+    .action(function () {
+        getUsers();
+    });
+
+program.parse();
+
+// program.command('split')
+//     .description('Split a string into substrings and display as an array')
+//     .argument('<string>', 'string to split')
+//     .option('--first', 'display just the first substring')
+//     .option('-s, --separator <char>', 'separator character', ',')
+//     .action(function (str, options) {
+//         var limit = options.first ? 1 : undefined;
+//         console.log(str.split(options.separator, limit));
+//     });
