f35a138b6c2b5c8e1bf2631b0dcb188445b310a4
linh <hoquanglinh2710@gmail.com>
2024-03-07 17:06:46 +0700

    Add help desk client schema

---
+++ src/helpdesk/schemas/help-desk-client.schema.ts
@@ -0,0 +1,38 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from 'src/users/schemas/user.schema';
+import { HelpDeskBusiness } from './help-desk-business.schema';
+
+@Schema({
+  timestamps: true,
+})
+export class HelpDeskClient {
+  _id: mongoose.Schema.Types.ObjectId;
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: HelpDeskBusiness.name,
+    required: true,
+  })
+  business: HelpDeskBusiness;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User.name,
+  })
+  user: User;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String })
+  phoneNumber: string;
+
+  @Prop({ type: String, required: true })
+  language: string;
+}
+
+export const HelpDeskClientSchema =
+  SchemaFactory.createForClass(HelpDeskClient);
