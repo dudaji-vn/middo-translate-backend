8ebfc6b41069d3660fd23be3cd55c9586aa68012
linh <hoquanglinh2710@gmail.com>
2024-03-07 17:03:16 +0700

    Update help desk module

--- src/helpdesk/help-desk.module.ts
+++ src/helpdesk/help-desk.module.ts
@@ -1,18 +1,26 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { HelpDesk, HelpDeskSchema } from './schemas/help-desk.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { NotificationModule } from 'src/notifications/notifications.module';
 import { RoomsModule } from 'src/rooms/rooms.module';
 import { UsersModule } from 'src/users/users.module';
+import { User, UserSchema } from '../users/schemas/user.schema';
 import { HelpDeskController } from './help-desk.controller';
 import { HelpDeskService } from './help-desk.service';
+import {
+  HelpDeskBusiness,
+  HelpDeskBusinessSchema,
+} from './schemas/help-desk-business.schema';
+import {
+  HelpDeskClient,
+  HelpDeskClientSchema,
+} from './schemas/help-desk-client.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: HelpDesk.name, schema: HelpDeskSchema },
+      { name: HelpDeskClient.name, schema: HelpDeskClientSchema },
+      { name: HelpDeskBusiness.name, schema: HelpDeskBusinessSchema },
+      { name: User.name, schema: UserSchema },
     ]),
-    NotificationModule,
     UsersModule,
     forwardRef(() => RoomsModule),
   ],
