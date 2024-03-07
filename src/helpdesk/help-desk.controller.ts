5edd27b485d212cf23a77ff6b6dacdea90c9561f
linh <hoquanglinh2710@gmail.com>
2024-03-07 17:07:35 +0700

    Add createClient and createOrEditBusiness api

--- src/helpdesk/help-desk.controller.ts
+++ src/helpdesk/help-desk.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtUserId, Public } from 'src/common/decorators';
+import { CreateClientDto } from './dto/create-client-dto';
+import { CreateOrEditBusinessDto } from './dto/create-or-edit-business-dto';
 import { HelpDeskService } from './help-desk.service';
 
-@Controller('messages')
+@ApiTags('help-desk')
+@Controller('help-desk')
 export class HelpDeskController {
   constructor(private readonly helpDeskService: HelpDeskService) {}
+  @Public()
+  @Post('create-client')
+  async createClient(@Body() client: CreateClientDto) {
+    const result = await this.helpDeskService.createClient(
+      client.businessId,
+      client,
+    );
+    return { data: result };
+  }
+
+  @Put('create-or-edit-business')
+  async createOrEditBusiness(
+    @JwtUserId() userId: string,
+    @Body() business: CreateOrEditBusinessDto,
+  ) {
+    const result = await this.helpDeskService.createOrEditBusiness(
+      userId,
+      business,
+    );
+    return { data: result };
+  }
 }
--- src/helpdesk/help-desk.service.ts
+++ src/helpdesk/help-desk.service.ts
@@ -1,17 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { RoomsService } from 'src/rooms/rooms.service';
+import { User, UserStatus } from 'src/users/schemas/user.schema';
 import { UsersService } from 'src/users/users.service';
-import { HelpDesk } from './schemas/help-desk.schema';
+import { HelpDeskBusiness } from './schemas/help-desk-business.schema';
+import { HelpDeskClient } from './schemas/help-desk-client.schema';
 
 @Injectable()
 export class HelpDeskService {
   constructor(
-    private readonly usersService: UsersService,
-    private readonly roomsService: RoomsService,
-    private readonly eventEmitter: EventEmitter2,
-    @InjectModel(HelpDesk.name) private helpDeskModel: Model<HelpDesk>,
+    @InjectModel(HelpDeskClient.name)
+    private helpDeskClientModel: Model<HelpDeskClient>,
+    @InjectModel(HelpDeskBusiness.name)
+    private helpDeskBusinessModel: Model<HelpDeskBusiness>,
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private userService: UsersService,
   ) {}
+
+  async createClient(businessId: string, info: Partial<HelpDeskClient>) {
+    const { email = '' } = info;
+    const isEmailExist = await this.userService.isEmailExist(email);
+    if (isEmailExist) {
+      throw new BadRequestException(
+        'Email is exist. Please login with Middo account',
+      );
+    }
+    const business = await this.helpDeskBusinessModel.findById(businessId);
+    if (!business) {
+      throw new BadRequestException('Business not found');
+    }
+    const user = await this.userModel.create({
+      status: UserStatus.PENDING,
+      email: `${new Date().getTime()}@gmail.com`,
+    });
+    info.user = user;
+    info.business = business;
+    const helpDeskClient = await this.helpDeskClientModel.create(info);
+    return helpDeskClient;
+  }
+
+  async createOrEditBusiness(userId: string, info: Partial<HelpDeskBusiness>) {
+    info.user = userId;
+    const user = await this.helpDeskBusinessModel.findOneAndUpdate(
+      {
+        user: userId,
+      },
+      info,
+      { new: true, upsert: true },
+    );
+    return user;
+  }
 }
