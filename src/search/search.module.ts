9dd892b4a29462b8be2b04b0108a4a6316fb61f7
linh <hoquanglinh2710@gmail.com>
2024-06-10 14:56:46 +0700

    Update search module

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -4,9 +4,16 @@ import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 import { UsersModule } from 'src/users/users.module';
 import { HelpDeskModule } from '../help-desk/help-desk.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Search, SearchSchema } from './schemas/search.schema';
 
 @Module({
-  imports: [UsersModule, RoomsModule, HelpDeskModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Search.name, schema: SearchSchema }]),
+    UsersModule,
+    RoomsModule,
+    HelpDeskModule,
+  ],
   controllers: [SearchController],
   providers: [SearchService],
 })
