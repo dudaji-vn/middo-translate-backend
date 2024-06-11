2c7466ab01aff5aaf37be9e77d13305c938524b2
linh <hoquanglinh2710@gmail.com>
2024-06-11 17:04:52 +0700

    Add type of messages response

--- src/search/types/search-main-result.type.ts
+++ src/search/types/search-main-result.type.ts
@@ -1,8 +1,9 @@
 import { Room } from 'src/rooms/schemas/room.schema';
 import { User } from 'src/users/schemas/user.schema';
+import { Message } from 'src/messages/schemas/messages.schema';
 
 export type SearchMainResult = {
   users: User[];
   rooms: Room[];
-  messages: any;
+  messages: Message[];
 };
